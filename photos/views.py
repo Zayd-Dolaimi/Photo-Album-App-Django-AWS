e6902dff09abca8bd6e6709522fda192a67c2956
Zayd Al-Dolaimi <zaid.1991@yahoo.com>
2023-05-09 01:48:22 +0300

    First Commit

---
+++ photos/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from .models import Category, Photo
+
+# Create your views here.
+def gallery(request):
+    category = request.GET.get('category')
+    if category == None:
+        photos = Photo.objects.all()
+    else:
+        photos = Photo.objects.filter(category__name=category)
+
+    categories = Category.objects.all()
+    context = {'categories': categories, 'photos': photos}
+    return render(request, 'photos/gallery.html', context)
+
+def viewphoto(request, pk):
+    photo = Photo.objects.get(id=pk)
+    return render(request, 'photos/photo.html', {'photo':photo})
+
+def addphoto(request):
+    return render(request, 'photos/add.html')
